126e8749df0336ff998b8f25b4505d0d244df3bd
gaboy1977 <81317643+gaboy1977@users.noreply.github.com>
2025-07-17 20:26:29 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('send-btn').addEventListener('click', function() {
-    const chatInput = document.getElementById('chat-input');
-    const chatBox = document.getElementById('chat-box');
-    const message = chatInput.value;
-    chatBox.innerHTML += `<p>You: ${message}</p>`;
-    chatInput.value = '';
-    // Here you would implement the logic to send the message and receive responses
-});
